54c21485ca9ef208a28b3cefd74297abbb439ef1
Gene <genedanielkim@gmail.com>
2022-12-26 13:05:40 -0800

    Added for loop to display similar movie results

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,18 +6,23 @@ $("#search-button").click(function () {
   let title = $("#movie-title").val();
   title = title.split(" ").join("+");
   console.log(title);
-  $.ajax(`http://www.omdbapi.com/?apikey=beff67b&t=${title}`).then(function (
-    response
-  ) {
+  $.ajax(`http://www.omdbapi.com/?apikey=beff67b&t=${title}`).then(function (response) {
     console.log(response);
     $("#movie-poster").attr("src", response.Poster);
     imdbId = response.imdbID;
 
-    $.ajax(
-      `https://api.themoviedb.org/3/movie/${imdbId}/similar?api_key=43ba0a31020fe2244998abeaf52535a4&language=en-US&page=1`
-    ).then(function (similarMovies) {
-      posterPath = similarMovies.poster_path;
+    $.ajax(`https://api.themoviedb.org/3/movie/${imdbId}/similar?api_key=43ba0a31020fe2244998abeaf52535a4&language=en-US&page=1`).then(function (similarMovies) {
+
       console.log(similarMovies);
+
+      $('.similar-movies').empty();
+
+      for (var i = 0; i < similarMovies.results.length; i++) {
+        var fullUrl = posterUrl + similarMovies.results[i].poster_path;
+        //console.log(fullUrl);
+        
+        $('.similar-movies').append(`<img id='similar-${i}' src='${fullUrl}'/>`);
+      }
     });
   });
 });
