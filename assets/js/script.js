ea1d94f0c5231c757dbba98419484a8c8a218efc
Collins Curran <currancollins22@gmail.com>
2023-01-01 17:46:52 -0800

    added JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,22 +6,27 @@ $("#search-button").click(function () {
   let title = $("#movie-title").val();
   title = title.split(" ").join("+");
   console.log(title);
-  $.ajax(`http://www.omdbapi.com/?apikey=beff67b&t=${title}`).then(function (response) {
+  $.ajax(`http://www.omdbapi.com/?apikey=beff67b&t=${title}`).then(function (
+    response
+  ) {
     console.log(response);
     $("#movie-poster").attr("src", response.Poster);
     imdbId = response.imdbID;
 
-    $.ajax(`https://api.themoviedb.org/3/movie/${imdbId}/similar?api_key=43ba0a31020fe2244998abeaf52535a4&language=en-US&page=1`).then(function (similarMovies) {
-
+    $.ajax(
+      `https://api.themoviedb.org/3/movie/${imdbId}/similar?api_key=43ba0a31020fe2244998abeaf52535a4&language=en-US&page=1`
+    ).then(function (similarMovies) {
       console.log(similarMovies);
 
-      $('.similar-movies').empty();
+      $(".similar-movies").empty();
 
       for (var i = 0; i < similarMovies.results.length; i++) {
         var fullUrl = posterUrl + similarMovies.results[i].poster_path;
         //console.log(fullUrl);
-        
-        $('.similar-movies').append(`<img id='similar-${i}' src='${fullUrl}'/>`);
+
+        $(".similar-movies").append(
+          `<img id='similar-${i}' src='${fullUrl}'/>`
+        );
       }
     });
   });
