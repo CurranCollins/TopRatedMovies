fbf5184d918777b1cc9c5715bb3be1b599a66662
Collins Curran <currancollins22@gmail.com>
2023-01-04 18:20:22 -0800

    hopefully fixed live publish

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,7 +24,7 @@ $("#search-button").click(function () {
   console.log(localStorage);
   title = title.split(" ").join("+");
 
-  $.ajax(`http://www.omdbapi.com/?apikey=beff67b&t=${title}`).then(function (
+  $.ajax(`https://www.omdbapi.com/?apikey=beff67b&t=${title}`).then(function (
     response
   ) {
     if (response.Response == "False") {
@@ -43,7 +43,7 @@ $("#search-button").click(function () {
           searchHistory.shift();
         }
 
-        for(let i = 0; i < searchHistory.length; i++) {
+        for (let i = 0; i < searchHistory.length; i++) {
           $("#saved-searches").prepend(
             `<button class="button history-btn">${searchHistory[i]}</button>`
           );
@@ -95,9 +95,9 @@ $("#search-button").click(function () {
   });
 });
 
-$("#saved-searches").on('click', '.history-btn', function (event) {
+$("#saved-searches").on("click", ".history-btn", function (event) {
   $.ajax(
-    `http://www.omdbapi.com/?apikey=beff67b&t=${event.target.innerHTML}`
+    `https://www.omdbapi.com/?apikey=beff67b&t=${event.target.innerHTML}`
   ).then(function (response) {
     if (response.Response == "False") {
       $("#searched-header").text(
